7ab7e6c0e414a97de034c515978cb43090651c38
ShadowObj <noreply@github.com>
2023-10-13 18:23:02 +0800

    Fixed Bug at /app/cmd/bedrock.go:59:63

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ go env -w GOARCH=amd64
 go env -w GOOS=linux
 go env -w GOAMD64=v3
 go env -w CGO_ENABLED=0
-go build -o hysteria_linux_amd64_avx_v1.3.72 -tags=gpl -ldflags "-s -w -X 'main.appDate=2023-10-04 12:01:01' -X 'main.appVersion=1.3.72'  -X 'main.appCommit=2e16fd6'" -trimpath ./app/cmd/
+go build -o hysteria_linux_amd64_avx_v1.3.74 -tags=gpl -ldflags "-s -w -X 'main.appDate=2023-10-13 18:01:01' -X 'main.appVersion=1.3.74'  -X 'main.appCommit=d085854'" -trimpath ./app/cmd/
